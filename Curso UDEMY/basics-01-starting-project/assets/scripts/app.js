be893bf668a1e4ef8185771cc715fbe3d198c896
Agustin de Zuviria Allende <agustinzuviria@gmail.com>
2022-04-26 22:37:52 -0300

    appjs old version commented

--- Curso UDEMY/basics-01-starting-project/assets/scripts/app.js
+++ Curso UDEMY/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,33 @@
+const defaultResult = 0
 let currentResult = 0;
+
+function getUserNumberInput() {
+    return parseInt(userInput.value)
+}
+function calculateText (beforeCalc, operator, calcNum) {
+    `${beforeCalc} ${operator} ${calcNum}`
+}
+
+
+
+//PROBLEM: Function calculateText is giving me a number instead of a string 
+// chapter 39 of Udemy
+
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = calculateText (currentResult, "+", enteredNumber);
+    currentResult = currentResult + enteredNumber; 
+    outputResult (currentResult, calcDescription)
+    }
+    cd
+    addBtn.addEventListener("click", add);
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = calculateText (currentResult, "*", enteredNumber);
+    currentResult = currentResult * enteredNumber; 
+    outputResult (currentResult, calcDescription)
+    }
+    
+    multiplyBtn.addEventListener("click", multiply);
