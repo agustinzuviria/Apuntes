158e79459abe36fadfa0f027f060156141fe0214
Agustin de Zuviria Allende <agustinzuviria@gmail.com>
2022-05-12 16:47:07 -0300

    Finished exc 88

--- Curso UDEMY/control-04-attack-function/assets/scripts/app.js
+++ Curso UDEMY/control-04-attack-function/assets/scripts/app.js
@@ -4,16 +4,25 @@ const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
 
-let chosenMaxLife = 50;
+let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage (MONSTER_ATTACK_VALUE)
 currentPlayerHealth -= playerDamage;
-  if(currentMonsterHealth <=0 && currentPlayerHealth>0) {
+alert(currentPlayerHealth);
+
+if (currentPlayerHealth <=0 && hasBonusLife) {
+  hasBonusLife = false;
+  removeBonusLife();
+  setPlayerHealth(initialPlayerHealth);
+  alert("You would be dead, but the bonus life saved you!")
+} else if(currentMonsterHealth <=0 && currentPlayerHealth>0) {
     alert("you won");
   } else if(currentPlayerHealth <=0 && currentMonsterHealth>0) {
     alert("you lose")
@@ -42,7 +51,6 @@ function healPlayerHandler () {
   currentPlayerHealth += HEAL_VALUE
   }
   endRound();
-  alert(currentPlayerHealth)
 }
 
 function attackHandler() { 
