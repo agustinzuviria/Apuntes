07b7aef3554f3f88ec1e303f375d6b2eaa02d753
Agustin de Zuviria Allende <agustinzuviria@gmail.com>
2022-05-12 15:56:15 -0300

    Exc 86 finished

--- Curso UDEMY/control-04-attack-function/assets/scripts/app.js
+++ Curso UDEMY/control-04-attack-function/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 50;
@@ -7,11 +8,17 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster (mode) {
+  let maxDamage
+if (mode === "ATTACK") {
+  maxDamage = ATTACK_VALUE
+} else {
+  maxDamage = STRONG_ATTACK_VALUE
+}
+const damage = dealMonsterDamage (maxDamage);
   currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
-currentPlayerHealth -= playerDamage
+  const playerDamage = dealPlayerDamage (MONSTER_ATTACK_VALUE)
+currentPlayerHealth -= playerDamage;
   if(currentMonsterHealth <=0 && currentPlayerHealth>0) {
     alert("you won");
   } else if(currentPlayerHealth <=0 && currentMonsterHealth>0) {
@@ -19,7 +26,16 @@ currentPlayerHealth -= playerDamage
   } else if (currentMonsterHealth <0 && currentPlayerHealth <0) {
     alert ("you have a draw")
   }
+}
 
+
+function attackHandler() { 
+  attackMonster ("ATTACK")
+}
+
+function strongAttackHandler() {
+  attackMonster ("STRONG_ATTACK")  
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler)
