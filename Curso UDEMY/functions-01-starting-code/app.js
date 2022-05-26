79a89939f7945f37d97657c230f8f05b09821220
Agustin de Zuviria Allende <agustinzuviria@gmail.com>
2022-05-25 21:10:17 -0300

    starting exc rock paper scissors

--- Curso UDEMY/functions-01-starting-code/app.js
+++ Curso UDEMY/functions-01-starting-code/app.js
@@ -1,1 +1,61 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = "ROCK"
+const PAPER = "PAPER"
+const SCISSORS = "SCISSORS"
+const DEFAULT_USER_CHOICE = ROCK
+const RESULT_DRAW = "DRAW"
+const RESULT_PLAYER_WINS = "PLAYER WINS"
+const RESULT_COMPUTER_WINS = "COMPUTER WINS"
+
+let gameIsRunning = false
+
+const getPlayerChoice = function() {
+    const selection = prompt( `${ROCK}, ${PAPER} or ${SCISSORS}?`,"").toUpperCase();
+    if (
+selection !== ROCK &&
+selection !== PAPER &&
+selection !== SCISSORS
+    ) {
+       alert (`Invalidad choice! We chose ${DEFAULT_USER_CHOICE} for you`);
+    return DEFAULT_USER_CHOICE;
+    }
+return selection;
+}
+
+const getComputerChoice = function() {
+let randomNum = Math.random();
+if (randomNum < 0.33) {
+    return ROCK;
+} else if (randomNum > 0.33) {
+   return PAPER; 
+} else {
+    return SCISSORS
+}
+}
+
+const getWinner = function(cChoise, pChoise) {
+    if (cChoise === pChoise) {
+        return RESULT_DRAW
+    } else if (
+        cChoise === ROCK && pChoise === SCISSORS || 
+        cChoise === SCISSORS && pChoise === PAPER || 
+        cChoise === PAPER && pChoise === ROCK  ) {
+        return RESULT_COMPUTER_WINS
+    } else {
+        return RESULT_PLAYER_WINS
+    }
+    }
+
+startGameBtn.addEventListener ("click", function() {
+if(gameIsRunning){
+    return;
+}
+
+    gameIsRunning = true
+    console.log ("The game is starting!");
+    const playerChoise = getPlayerChoice();
+    const computerChoise = getComputerChoice();
+    const winner = getWinner(computerChoise, playerChoise);
+    console.log(winner);
+});
